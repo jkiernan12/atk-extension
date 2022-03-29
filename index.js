8fcf3c7ece409ef72e7bad3b7f4e2da2e304ee26
John Kiernan <jkiernan12@yahoo.com>
2022-03-29 13:38:38 -0400

    Add model num and price

--- index.js
+++ index.js
@@ -6,12 +6,23 @@ let content = document.querySelectorAll('.reviewable-detail__reviewables > .acco
 console.log(content)
 
 let items = Array.from(content).map(item => {
+  let price = null
+  let modelNum = null
+  item.querySelectorAll('div').forEach(el => {
+    if (el.innerHTML.includes('Price at Time of Testing')) {
+      price = el.textContent.replace('Price at Time of Testing: ', '')
+    } else if (el.innerHTML.includes('Model Number:')) {
+      modelNum = el.textContent.replace('Model Number: ', '')
+    }
+  })
   let status = item.parentNode.childNodes[0].textContent
   let secondaryStatus = Array.from(item.childNodes[0].childNodes[0].childNodes[0].childNodes[0].childNodes).find(el => el.classList.contains('reviewable-sticker-wrapper'))?.childNodes[0].textContent
   return {
     brand: Array.from(item.childNodes[0].childNodes[0].childNodes[0].childNodes[0].childNodes).find(el => el.nodeName === 'H3').textContent,
     status: status,
-    secondaryStatus: secondaryStatus ? secondaryStatus : null
+    secondaryStatus: secondaryStatus ? secondaryStatus : null,
+    modelNum: modelNum,
+    price: price
   }
 })
 console.log(items)
